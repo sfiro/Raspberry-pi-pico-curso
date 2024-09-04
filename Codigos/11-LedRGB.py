df827b608bf8e175f944ba76865bcd9c2d033cc5
Debbie Arredondo <chevi6@gmail.com>
2024-09-04 13:32:11 -0500

    se corrige el codigo del brillo de led

--- Codigos/11-LedRGB.py
+++ Codigos/11-LedRGB.py
@@ -7,46 +7,24 @@ green = PWM(Pin(0))
 blue = PWM(Pin(1))
 red = PWM(Pin(2))
 
-tiempo = 0.1
+tiempo = 0.01
 
-# Función para aumentar el brillo del LED rojo
-def rojo():
+# Función para aumentar el brillo del LED 
+def brillo(objeto):
     for pwm in range(0, 65000, 1000):  # Recorre valores de 1000 a 65000 con incrementos de 1000
-        red.duty_u16(pwm)                 # Establece el ciclo de trabajo del PWM del LED rojo
-        time.sleep(tiempo)                  # Pausa de 10 milisegundos para un aumento gradual del brillo
+        objeto.duty_u16(pwm)                 # Establece el ciclo de trabajo del PWM del LED 
+        time.sleep(tiempo)                  # Pausa en milisegundos para un aumento gradual del brillo
     
     for pwm in range(65000, 0, -1000):  # Recorre valores de 1000 a 65000 con incrementos de 1000
-        red.duty_u16(pwm)                 # Establece el ciclo de trabajo del PWM del LED rojo
-        time.sleep(tiempo)                  # Pausa de 10 milisegundos para un aumento gradual del brillo
-
-# Función para aumentar el brillo del LED verde
-def verde():
-    for pwm in range(0, 65000, 1000):  # Recorre valores de 1000 a 65000 con incrementos de 1000
-        green.duty_u16(pwm)               # Establece el ciclo de trabajo del PWM del LED verde
-        time.sleep(tiempo)                  # Pausa de 10 milisegundos para un aumento gradual del brillo
-    
-    for pwm in range(65000, 0, -1000):  # Recorre valores de 1000 a 65000 con incrementos de 1000
-        green.duty_u16(pwm)               # Establece el ciclo de trabajo del PWM del LED verde
-        time.sleep(tiempo)                  # Pausa de 10 milisegundos para un aumento gradual del brillo
-
-# Función para aumentar el brillo del LED azul
-def azul():
-    for pwm in range(0, 65000, 1000):  # Recorre valores de 1000 a 65000 con incrementos de 1000
-        blue.duty_u16(pwm)                # Establece el ciclo de trabajo del PWM del LED azul
-        time.sleep(tiempo)                  # Pausa de 10 milisegundos para un aumento gradual del brillo
-
-    for pwm in range(65000, 0, -1000):  # Recorre valores de 1000 a 65000 con incrementos de 1000
-        blue.duty_u16(pwm)               # Establece el ciclo de trabajo del PWM del LED verde
-        time.sleep(tiempo)                  # Pausa de 10 milisegundos para un aumento gradual del brillo
+        objeto.duty_u16(pwm)                 # Establece el ciclo de trabajo del PWM del LED 
+        time.sleep(tiempo)                  # Pausa en milisegundos para un aumento gradual del brillo
 
 # Bucle principal que se ejecuta indefinidamente
 while True:
     red.duty_u16(0)                 # Establece el ciclo de trabajo del PWM del LED rojo
     green.duty_u16(0)                 # Establece el ciclo de trabajo del PWM del LED rojo
     blue.duty_u16(0)                 # Establece el ciclo de trabajo del PWM del LED rojo
-    rojo()            # Llama a la función para aumentar el brillo del LED rojo
-    time.sleep(1)     # Pausa de 1 segundo después de completar la secuencia roja
-    verde()           # Llama a la función para aumentar el brillo del LED verde
-    time.sleep(1)     # Pausa de 1 segundo después de completar la secuencia verde
-    azul()            # Llama a la función para aumentar el brillo del LED azul
-    time.sleep(1)     # Pausa de 1 segundo después de completar la secuencia azul
+
+    brillo(red)      # Llama a la función para aumentar el brillo del LED 
+    brillo(green)           # Llama a la función para aumentar el brillo del LED 
+    brillo(blue)            # Llama a la función para aumentar el brillo del LED
